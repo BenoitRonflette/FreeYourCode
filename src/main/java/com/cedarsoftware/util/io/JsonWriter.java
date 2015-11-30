e5f7ba45cff84183da1a3a83f4b3c715ed1cb447
unknown <BRE@BRE-D3800.akuiteo.lan>
2015-11-30 18:06:23 +0100

    prob when map index are not strings

--- src/main/java/com/cedarsoftware/util/io/JsonWriter.java
+++ src/main/java/com/cedarsoftware/util/io/JsonWriter.java
@@ -1992,7 +1992,7 @@ public class JsonWriter implements Closeable, Flushable
         {
             Entry att2value = (Entry) i.next();
             output.write("\"");
-            output.write((String) att2value.getKey());
+            output.write(String.valueOf(att2value.getKey()));
             output.write("\":");
 
             writeCollectionElement(att2value.getValue());
